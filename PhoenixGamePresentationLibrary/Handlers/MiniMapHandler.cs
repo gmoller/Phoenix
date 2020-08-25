1df4bb613a86132550e1a08ed038554771c9f435
Greg Moller <greg.moller@gmail.com>
2020-08-25 15:23:27 -0700

    Minimap work

--- PhoenixGamePresentationLibrary/Handlers/MiniMapHandler.cs
+++ PhoenixGamePresentationLibrary/Handlers/MiniMapHandler.cs
@@ -14,12 +14,15 @@ namespace PhoenixGamePresentationLibrary.Handlers
             var graphicsDevice = (GraphicsDevice)context.GraphicsDevice;
             var terrainTypes = context.GameMetadata.TerrainTypes;
 
+            var scalingFactor = 2;
             var cellGrid = world.OverlandMap.CellGrid;
             var width = cellGrid.NumberOfColumns;
+            var scaledWidth = width * scalingFactor;
             var height = cellGrid.NumberOfRows;
-            var minimap = new Texture2D(graphicsDevice, width, height, false, SurfaceFormat.Color);
+            var scaledHeight = height * scalingFactor;
+            var minimap = new Texture2D(graphicsDevice, scaledWidth, scaledHeight, false, SurfaceFormat.Color);
 
-            var colors = new Color[width * height];
+            var colors = new Color[scaledWidth * scaledHeight];
             var i = 0;
             for (var row = 0; row < height; row++)
             {
@@ -28,9 +31,24 @@ namespace PhoenixGamePresentationLibrary.Handlers
                     var cell = cellGrid.GetCell(column, row);
                     var terrainTypeId = cell.TerrainTypeId;
                     var color = cell.SeenState == SeenState.NeverSeen ? Utilities.Color.Black :  terrainTypes[terrainTypeId].MinimapColor;
-                    colors[i] = new Color(color.R, color.G, color.B, color.A);
-                    i++;
+
+                    var index = i;
+                    colors[index] = new Color(color.R, color.G, color.B, color.A);
+                    index = i + 1;
+                    colors[index] = new Color(color.R, color.G, color.B, color.A);
+
+                    index = i + scaledWidth;
+                    if (index < colors.Length)
+                    {
+                        colors[index] = new Color(color.R, color.G, color.B, color.A);
+                        index = i + 1 + scaledWidth;
+                        colors[index] = new Color(color.R, color.G, color.B, color.A);
+                    }
+
+                    i += scalingFactor;
                 }
+
+                i += scaledWidth + (row % 2 == 0 ? 1 : 0);
             }
 
             minimap.SetData(colors);
